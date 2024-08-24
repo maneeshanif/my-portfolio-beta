7887eba905a3af85a19ec455538cc8d97763b77d
maneeshanif <manees7865@gmail.com>
2024-08-24 16:03:53 +0500

    logical error fixed🥲

---
+++ dist/index.e698439d.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",()=>{let e=document.querySelector(".curtain"),d=document.querySelector(".heading"),t=document.querySelector(".loader");setTimeout(()=>{t.classList.add("hidden"),e.classList.add("open"),setTimeout(()=>{d.classList.add("show")},500)},2e3)});
+//# sourceMappingURL=index.e698439d.js.map
